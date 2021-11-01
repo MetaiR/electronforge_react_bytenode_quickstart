a76b47a1087623d6a0ada3aaf52cf672d0757307
Seyed Ali Roshan <Seyedalir@Yahoo.com>
2021-11-02 03:15:00 +0330

    ref: change a text

--- src/react/components/test/test.tsx
+++ src/react/components/test/test.tsx
@@ -4,7 +4,7 @@ import {Component, ReactNode} from "react";
 export class Test extends Component {
     render(): ReactNode {
         return (
-            <h2>Hello World!</h2>
+            <h2>I am Test Route!</h2>
         );
     }
 }
